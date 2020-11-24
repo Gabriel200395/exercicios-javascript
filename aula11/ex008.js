7669e3149e19c81ac2e6d001d538e8aab247d5c5
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-11-24 14:55:28 -0300

    exercicio da aula 11 Feito

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var velocidade = 60.4;
+if (velocidade > 60) {
+  // condição simples
+  console.log("você passou do limete de velocide. MULTADO!");
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = "Brasil";
+if (pais === "Brasil") {
+  console.log("você e brasileiro");
+} else {
+  console.log("você e estrangeiro");
+}
