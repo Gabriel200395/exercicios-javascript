92b2e94ce26193a330c260d9291056578f14044f
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-11-24 17:32:33 -0300

    Aula - 14 Concluida

---
+++ aula-14 - repeticoes/ex0012.js
@@ -0,0 +1,15 @@
+/*
+var c = 1;
+while (c <= 8) {
+  console.log(`passo a passo${c}`);
+  c++;
+}
+do {
+  console.log(`passo a passo${c}`);
+  c++;
+} while (c <= 8);
+*/
+
+for (c = 1; c <= 8; c++) {
+  console.log("passo a passo" + c);
+}
