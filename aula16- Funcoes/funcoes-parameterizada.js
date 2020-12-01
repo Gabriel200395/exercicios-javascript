917f9ac8fba3b68912cd33b2c909d72e39bdb844
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-12-01 14:38:09 -0300

    Projeto 4 finalizado

---
+++ aula16- Funcoes/funcoes-parameterizada.js
@@ -0,0 +1,46 @@
+let Gabriel = {
+  idade: 25,
+  estadoCivil: "casado",
+  tempoCasado: 2,
+};
+
+function detalheGabriel(Gabriel) {
+  if (Gabriel.idade > 25) {
+    console.log("idade não esta correta");
+  } else {
+    console.log("idade esta correta");
+  }
+  return Gabriel;
+}
+
+detalheGabriel(Gabriel);
+
+/*
+let autores = [
+  (autorA = {
+    nome: "React Js",
+    data: "20 / 03 / 1998",
+    editora: "Pearson",
+  }),
+  (autorB = {
+    nome: "React Js",
+    data: "30 / 04 / 2000",
+    editora: "Pearson",
+  }),
+  (autorC = {
+    nome: "React Js",
+    data: "20 / 03 / 98",
+    editora: "Pearson",
+  }),
+];
+
+function buscarData(autores) {
+  if (autores.nome === "React Js" && autores.editora === "Pearson") {
+    console.log("O nome do livro esta correto");
+  } else {
+    console.log("O nome do livro esta errado");
+  }
+}
+
+buscarData(autores[0]);
+*/
