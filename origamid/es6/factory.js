c23d932c1e011b822f711efe83cf3a91c08c0683
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2021-02-10 11:48:34 -0300

    terminando sobre Objetos

--- origamid/es6/factory.js
+++ origamid/es6/factory.js
@@ -4,10 +4,25 @@ function criElement(text) {
     criarButton.innerHTML = text;
     return criarButton;
   }
-  return {
+  // congelar meus objetos
+  return Object.freeze({
     text,
     element,
-  };
+  });
 }
 
 const btnBlue = criElement("Olá eu sou button!");
+
+function Pessoa(nome) {
+  if (!(this instanceof Pessoa)) {
+    return new Pessoa(nome);
+  }
+  this.nome = nome;
+
+  Pessoa.prototype.pessoaNome = function () {
+    return console.log(" ele e designer " + this.nome);
+  };
+}
+
+const designer = Pessoa("Gabriel");
+console.log(designer);
